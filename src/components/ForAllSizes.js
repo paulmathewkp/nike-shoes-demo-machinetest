d61717a80dd9e4e468d06d49cba330f512b939f8
Paul Mathew <paulmathewkp@gmail.com>
2023-06-28 13:08:52 +0530

    image issue fixes

--- src/components/ForAllSizes.js
+++ src/components/ForAllSizes.js
@@ -9,11 +9,11 @@ const ForAllSizes = () => {
     <div>
       <h1>Available Colors</h1>
       <div className="available-color-wrapper">
-        <Link to={`/${"men-shoes"}/${"airred-jordan-3-valor-blue"}`}>
+        <Link to={`/${"men-shoes"}/${"nike-react-infinity-run-flyknit"}`}>
           <div className="test1">
             <img
               src={
-                "http://localhost:3000/static/media/nike-red.56ed19c9d3be44060182.jpg"
+                "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg"
               }
               alt="Shoe Site"
               className="logo-image"
@@ -22,11 +22,11 @@ const ForAllSizes = () => {
           </div>
         </Link>
         <div>
-          <Link to={`/${"men-shoes"}/${"airgreen-jordan-3-valor-blue"}`}>
-            <div className="green-shoes-spacing last-black">
-              <span className="last-black"><img
+          <Link to={`/${"men-shoes"}/${"michael-feburary-sk8-hi"}`}>
+            <div className="green-shoes-spacing test1">
+              <span><img
                 src={
-                  "http://localhost:3000/static/media/nike-green.38606a65db2eb8b6cf65.jpg"
+                  "https://images.vans.com/is/image/Vans/MV122M-HERO?$583x583$"
                 }
                 alt="Shoe Site"
                 className="logo-image"
@@ -36,6 +36,7 @@ const ForAllSizes = () => {
           </Link>{" "}
           <Link to={`/${"men-shoes"}/${"nikenew-react-miler"}`}>
             <div className="green-shoes-spacing">
+            <div className="test1">
               <img
                 src={
                   "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-5cc7de3b-2afc-49c2-a1e4-0508997d09e6/react-miler-mens-running-shoe-DgF6nr.jpg"
@@ -44,6 +45,7 @@ const ForAllSizes = () => {
                 className="logo-image"
                 onClick={handleClick}
               />
+              </div>
             </div>
           </Link>{" "}
         </div>
